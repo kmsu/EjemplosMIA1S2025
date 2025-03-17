26ade043948f9acb290949e296e7912904a709c2
kmsu <ingkevins@gmail.com>
2025-03-17 01:32:25 -0600

    Reporte disco

--- Proyecto/main.go
+++ Proyecto/main.go
@@ -9,7 +9,34 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	//instalar go get -u github.com/rs/cors en la raiz del proyecto
 )
+
+type Entrada struct {
+	Text string `json:"text"`
+}
+
+type StatusResponse struct {
+	Message string `json:"message"`
+	Type    string `json:"type"`
+}
+
+/*func main() {
+	//EndPoint
+	http.HandleFunc("/analizar", getCadenaAnalizar)
+
+	// Configurar CORS con opciones predeterminadas
+	//Permisos para enviar y recir informacion
+	c := cors.Default()
+
+	// Configurar el manejador HTTP con CORS
+	handler := c.Handler(http.DefaultServeMux)
+
+	// Iniciar el servidor en el puerto 8080
+	fmt.Println("Servidor escuchando en http://localhost:8080")
+	http.ListenAndServe(":8080", handler)
+
+}*/
 
 func main() {
 	// MENSAJES DE INICIO
@@ -34,9 +61,67 @@ func main() {
 	}
 }
 
+/*
+func getCadenaAnalizar(w http.ResponseWriter, r *http.Request) {
+	var respuesta string
+	// Configurar la cabecera de respuesta
+	w.Header().Set("Content-Type", "application/json")
+
+	var status StatusResponse
+	//verificar que sea un post
+	if r.Method == http.MethodPost {
+		var entrada Entrada
+		if err := json.NewDecoder(r.Body).Decode(&entrada); err != nil {
+			http.Error(w, "Error al decodificar JSON", http.StatusBadRequest)
+			status = StatusResponse{Message: "Error al decodificar JSON", Type: "unsucces"}
+			json.NewEncoder(w).Encode(status)
+			return
+		}
+
+		//creo un lector de bufer para el archivo
+		lector := bufio.NewScanner(strings.NewReader(entrada.Text))
+		//leer el archivo linea por linea
+		for lector.Scan() {
+			//Elimina los saltos de linea
+			if lector.Text() != "" {
+				//Divido por # para ignorar todo lo que este a la derecha del mismo
+				linea := strings.Split(lector.Text(), "#") //lector.Text() retorna la linea leida
+				if len(linea[0]) != 0 {
+					fmt.Println("\n*********************************************************************************************")
+					fmt.Println("Comando en ejecucion: ", linea[0])
+					respuesta += "***************************************************************************************************************************\n"
+					respuesta += "Comando en ejecucion: " + linea[0] + "\n"
+					respuesta += analizar(linea[0]) + "\n"
+				}
+				//Comentarios
+				if len(linea) > 1 && linea[1] != "" {
+					fmt.Println("#" + linea[1] + "\n")
+					respuesta += "#" + linea[1] + "\n"
+				}
+			}
+
+		}
+
+		//fmt.Println("Cadena recibida ", entrada.Text)
+		w.WriteHeader(http.StatusOK)
+
+		status = StatusResponse{Message: respuesta, Type: "succes"}
+		json.NewEncoder(w).Encode(status)
+
+	} else {
+		//http.Error(w, "Metodo no permitido", http.StatusMethodNotAllowed)
+		status = StatusResponse{Message: "Metodo no permitido", Type: "unsucces"}
+		json.NewEncoder(w).Encode(status)
+	}
+}
+*/
+
+// func analizar(entrada string) string {
 func analizar(entrada string) {
 	//Separar los parametros -size=3000 -path=ruta (obtenemos la lista: size=3000, path=ruta)
 	parametros := strings.Split(entrada, " -")
+
+	//respuesta := ""
 
 	//analizamos los parametros
 	if strings.ToLower(parametros[0]) == "execute" {
@@ -47,7 +132,7 @@ func analizar(entrada string) {
 				archivo, err := os.Open(tmpParametro[1])
 				if err != nil {
 					fmt.Println("Error al leer el script: ", err)
-					return
+					//return
 				}
 				defer archivo.Close()
 				//creo un lector de bufer para el archivo
@@ -104,8 +189,8 @@ func analizar(entrada string) {
 	} else if strings.ToLower(parametros[0]) == "rep" {
 		//REP
 		if len(parametros) > 1 {
-			//Comandos.Rep(parametros)
-			Comandos.Rep()
+			Comandos.Rep(parametros)
+			//Comandos.Rep()
 		} else {
 			fmt.Println("REP ERROR: parametros no encontrados")
 		}
@@ -119,4 +204,6 @@ func analizar(entrada string) {
 	} else {
 		fmt.Println("Comando no reconocible")
 	}
+
+	//return respuesta
 }
